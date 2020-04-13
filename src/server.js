87ad3af46a896ff855a7c217ffcc00b86e2edbf3
Tushar Raut <tusraut@publicisgroupe.net>
2020-04-13 13:00:32 +0530

    tga 2 commit

--- src/server.js
+++ src/server.js
@@ -1,36 +1,38 @@
-/* eslint-disable react/jsx-filename-extension */
+// /* eslint-disable react/jsx-filename-extension */
 import express from 'express'
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import { ServerStyleSheet } from 'styled-components'
+// import React from 'react'
+// import { renderToString } from 'react-dom/server'
+// import { ServerStyleSheet } from 'styled-components'
 
-import App from './client/App'
-import Layout from './client/Layout'
+// import App from './client/App'
+// import Layout from './client/Layout'
 
 const port = 3000;
 const server = express();
-server.use(express.static('dist'));
-server.use(express.static('images'));
+// server.use(express.static('dist'));
+// server.use(express.static('images'));
 
-function renderComponent(component) {
-  const sheet = new ServerStyleSheet();
+// function renderComponent(component) {
+//   const sheet = new ServerStyleSheet();
 
-  const body = renderToString(sheet.collectStyles(component)); // get styles
-  const styles = sheet.getStyleTags(); // getting all the tags from the sheet
+//   const body = renderToString(sheet.collectStyles(component)); // get styles
+//   const styles = sheet.getStyleTags(); // getting all the tags from the sheet
 
-  return {
-    body,
-    styles, // passing the styles to our Layout template
-  };
-}
+//   return {
+//     body,
+//     styles, // passing the styles to our Layout template
+//   };
+// }
 
-// Creating a single index route to server our React application from.
-server.get('/', (req, res) => {
-  const title = 'React Server Side Rendering with Styled Components';
-  const renderedComponent = renderComponent(<App />);
+// // Creating a single index route to server our React application from.
+// server.get('/', (req, res) => {
+//   const title = 'React Server Side Rendering with Styled Components';
+//   const renderedComponent = renderComponent(<App />);
 
-  res.send(Layout({ title, ...renderedComponent }));
+//   res.send(Layout({ title, ...renderedComponent }));
+// });
+server.get('/', function(req,res){
+  res.send('hello world');
 });
-
 server.listen(process.env.PORT || port, '0.0.0.0');
-console.log(`Serving at http://localhost:${port}`);
+// console.log(`Serving at http://localhost:${port}`);
